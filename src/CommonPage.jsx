7edcc51cc39f08a927affebaf3f9801e7646c247
Abhishek Singh <kashyapabhi688@gmail.com>
2021-06-10 23:45:56 +0530

    Changes into files for checking build error

--- src/CommonPage.jsx
+++ src/CommonPage.jsx
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 import web from '../src/assets/header_img.svg';
 
 
+
 const CommonPage =(props)=>{
   return(
     <React.Fragment>
--- src/helloWorld.js
+++ src/helloWorld.js
@@ -3,7 +3,7 @@ import { withLDConsumer } from 'launchdarkly-react-client-sdk';
 
 
 const HelloWorld = ({ flags }) => {
-    return flags.test ? <div>Flag on</div> : <div>Flag off</div>;
+    return flags.test ? <div><img src="https://cdn.pixabay.com/photo/2021/03/30/01/11/fife-6135609_960_720.jpg" alt="image" /> </div> : <div>""</div>;
 };
 
 export default withLDConsumer()(HelloWorld);
