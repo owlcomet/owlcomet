2b84788ad7a1cd20781b71d94cd56b1fdbfded7e
Abhishek Singh <kashyapabhi688@gmail.com>
2022-03-18 22:27:48 +0530

    minor tweak on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ const Home =()=>{
      <>
 <CommonPage name="One-stop solutions at " imgSrc={web} visit="/service" btnName="Get Started" 
 subText="We focus on the nature of your business and what you need for a fruitful online presence to ultimately push revenues further up."
-companyName="पंछी.com"
+companyName="owlcomets"
 />
     </>
   )
